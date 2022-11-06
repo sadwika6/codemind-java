bff700e96cd61491002806199e59fbbad1503a88
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-11-06 21:56:36 +0530

    import java.util.*; class Array {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         double a,s=0.0;         n=sc.nextInt();         int [] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();             s=s+arr[i];         }         a=s/n;         System.out.printf("%.2f",a);     } }

--- average_of_the_array.java
+++ average_of_the_array.java
@@ -1,19 +1,19 @@
-import java.util.Scanner;
-class sample
+import java.util.*;
+class Array
 {
     public static void main(String args[])
     {
+        Scanner sc=new Scanner(System.in);
         int n;
-        double a,s=0;
-        Scanner x=new Scanner(System.in);
-        n=x.nextInt();
-        int arr[]=new int[n];
+        double a,s=0.0;
+        n=sc.nextInt();
+        int [] arr=new int[n];
         for(int i=0;i<n;i++)
         {
-            arr[i]=x.nextInt();
+            arr[i]=sc.nextInt();
             s=s+arr[i];
         }
         a=s/n;
         System.out.printf("%.2f",a);
     }
-}
+} 
