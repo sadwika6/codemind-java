1525d51176f229b8214625232a339434bbd6fa7d
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-11-08 15:59:36 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         String s;         Scanner sc= new Scanner(System.in);         s=sc.nextLine();         System.out.print("Hello Technicalhub ");         System.out.print(s);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        String s;
+        Scanner sc= new Scanner(System.in);
+        s=sc.nextLine();
+        System.out.print("Hello Technicalhub
+");
+        System.out.print(s);
+    }
+}
