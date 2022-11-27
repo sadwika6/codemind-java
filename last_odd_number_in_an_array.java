440187e82693bf3b820ab3c1bb41bc67680e726f
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-11-27 15:41:32 +0530

    import java.util.*; class Array {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,c=0;         n=sc.nextInt();         int [] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();             if(arr[i]%2!=0)             {                 c=arr[i];             }         }         System.out.print(c);     } }

---
+++ last_odd_number_in_an_array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Array
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,c=0;
+        n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            if(arr[i]%2!=0)
+            {
+                c=arr[i];
+            }
+        }
+        System.out.print(c);
+    }
+} 
