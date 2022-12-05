3f6d689745fbfeb07a9b699e83de5b30f1b166ee
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-12-05 10:20:57 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         int n,a,b,f=0;         Scanner c=new Scanner(System.in);         n=c.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=c.nextInt();         }         a=c.nextInt();         b=c.nextInt();         for(int i=0;i<n;i++)         {             if(arr[i]<a || arr[i]>b)             {                 f=1;                 System.out.printf(arr[i]+" ");             }         }         if(f==0)         {             System.out.print("-1");         }     } }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        int n,a,b,f=0;
+        Scanner c=new Scanner(System.in);
+        n=c.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=c.nextInt();
+        }
+        a=c.nextInt();
+        b=c.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<a || arr[i]>b)
+            {
+                f=1;
+                System.out.printf(arr[i]+" ");
+            }
+        }
+        if(f==0)
+        {
+            System.out.print("-1");
+        }
+    }
+}
