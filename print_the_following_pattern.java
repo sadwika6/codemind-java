3566ae5a5506ce426b00f1f4320a449d73a5de6d
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-12-19 11:51:27 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         int n;         Scanner c=new Scanner(System.in);         n=c.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=n;j>=1;j--)             {                 System.out.print(j+" ");             }             System.out.print(' ');         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -6,21 +6,14 @@ class sample
         int n;
         Scanner c=new Scanner(System.in);
         n=c.nextInt();
-        for(int i=0;i<n;i++)
+        for(int i=1;i<=n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=n;j>=1;j--)
             {
-                if(i==j)
-                {
-                    System.out.print('0');
-                }
-                else
-                {
-                    System.out.print('x');
-                }
+                System.out.print(j+" ");
             }
-            System.out.print("
-");
+            System.out.print('
+');
         }
     }
 }
